c41abb81fc7fd08f2338f1cfe4eee31b0e077b1e
Jhon Perez <jhonaperez@unicauca.edu.co>
2026-03-01 17:27:49 -0500

    Creacion del INIT para el ejercicio 3 y sus respectivas secuencias

--- 1er corte/Practica1/Ejercicio 3 - Led Sequence/Ejercicio3_Led_Sequence.X/Ejercicio3_Led_Sequence.asm
+++ 1er corte/Practica1/Ejercicio 3 - Led Sequence/Ejercicio3_Led_Sequence.X/Ejercicio3_Led_Sequence.asm
@@ -20,38 +20,102 @@ CONFIG  XINST  = OFF          ; Juego de instrucciones extendido OFF
 CONFIG  PWRT   = ON           ; Espera un momento al encender antes de arrancar
     
 ; Vectores de Inicio e Interrupción
-	PSECT  resetVec, class=CODE, reloc=2
-        ORG     0b00000000          ; 0x00 Reset, cuando el micro se enciende o se resetea se va directico pa 0x0000
-        GOTO    INIT
-	
-	PSECT  intVec, class=CODE, reloc=2
-        ORG     0b00001000          ; 0x08 Vector de interrupción, al pisarse el boton se esto lo q hace es pausar lo que este haciendo el micro en ese momento para directamente a la nueva instruccion
-        GOTO    ISR
+    PSECT  resetVec, class=CODE, reloc=2
+    ORG     0b00000000          ; 0x00 Reset, cuando el micro se enciende o se resetea se va directico pa 0x0000
+    GOTO    INIT
+	 
+    PSECT  intVec, class=CODE, reloc=2
+    ORG     0b00001000          ; 0x08 Vector de interrupción, al pisarse el boton se esto lo q hace es pausar lo que este haciendo el micro en ese momento para directamente a la nueva instruccion
+    GOTO    ISR
 
 ; Inicio
 INIT:
-        ; OSCCON:
-        ; IRCF2:IRCF0 = 110 -> 4 MHz
-        ; SCS1:SCS0   = 10  -> reloj interno
-        ; Valor: 0b01100010
-        MOVLW   0b01100010
-        MOVWF   OSCCON, a
+    ; OSCCON:
+    ; IRCF2:IRCF0 = 111 -> 8 MHz
+    ; SCS1:SCS0   = 10  -> reloj interno
+    ; Valor: 0b01110010
+    MOVLW   0b01110010
+    MOVWF   OSCCON, a
 	
-	; LED RD0 salida, apagado
-	BCF     TRISD, 0, a
-        BCF     LATD,  0, a
+    ; LEDs como salidas, apagados
+    CLRF    TRISD, a        ; todo PORTD = salida (osea que son 0 bb, 0 es salida)
+    CLRF    LATD,  a        ; apaga todos los LEDs
 
-	; Botón RB0/INT0 como entrada
-        BSF     TRISB, 0, a
-	
-	; Pull-ups internos PORTB deshabilitados (RBPU=1)
-        ; (Con botón a VDD + pull-down externo NO se usan pull-ups)
-        BSF     INTCON2, 7, a
+    ; Botones como entradas
+    BSF     TRISB, 0, a     ; RB0 = entrada (botón secuencia)
+    BSF     TRISB, 1, a     ; RB1 = entrada (botón velocidad)
+    
+    ;Al presionarse el botón va de 5V a GND
+    BCF     INTCON2, 6, a   ; pone en 0 el bit 6 (INTEDG0)
+    BCF     INTCON2, 5, a   ; pone en 0 el bit 5 (INTEDG1)
+    
+    ;Limpiar banderas
+    BCF     INTCON,  1, a   ; limpia bandera de INT0
+    BCF     INTCON3, 0, a   ; limpia bandera de INT1
+    
+    ;Habilitamos interrupciones
+    BSF     INTCON,  4, a   ; =1, habilita INT0
+    BSF     INTCON3, 3, a   ; =1, habilita INT1
+    BSF     INTCON,  7, a   ; GIE=1, habilita interrupciones globales
+    
+MAIN:
+    MOVF    SEQ, W, a       ; carga SEQ en la mesa de trabajo (W)
+    XORLW   0               ; ¿SEQ es 0 o no mi so?
+    BZ      SECUENCIA_0     ; si sí, bien pueda siga a SECUENCIA_0
 
-	; IINT0 por flanco de BAJADA (INTEDG0=0) porque botón va a GND (1->0)
-        BCF     INTCON2, 6, a
+    MOVF    SEQ, W, a       ; vuelve a cargar SEQ (XORLW lo modifica)
+    XORLW   1               ; ¿SEQ es 1?
+    BZ      SECUENCIA_1
 
-        ; Habilitar INT0 + GIE
-        BCF     INTCON, 1, a	; INT0IF = 0
-        BSF     INTCON, 4, a	; INT0IE = 1
-        BSF     INTCON, 7, a	; GIE   = 1
+    MOVF    SEQ, W, a
+    XORLW   2               ; ¿SEQ es 2?
+    BZ      SECUENCIA_2
+
+    GOTO    SECUENCIA_3     ; si no fue 0,1,2 entonces es 3
+    
+    SECUENCIA_0:            ; Desplazamiento izq ? der
+	MOVLW   0x01
+	MOVWF   LATD, a     ; enciende RD0
+	CALL    DELAY
+	MOVLW   0x02
+	MOVWF   LATD, a     ; enciende RD1
+	CALL    DELAY
+	MOVLW   0x04
+	MOVWF   LATD, a     ; enciende RD2
+	CALL    DELAY
+	MOVLW   0x08
+	MOVWF   LATD, a     ; enciende RD3
+	CALL    DELAY
+	GOTO    MAIN
+
+    SECUENCIA_1:            ; Parpadeo total
+	MOVLW   0x0F
+	MOVWF   LATD, a     ; enciende todos
+	CALL    DELAY
+	CLRF    LATD, a     ; apaga todos
+	CALL    DELAY
+	GOTO    MAIN
+
+    SECUENCIA_2:            ; Relleno progresivo
+	MOVLW   0x01
+	MOVWF   LATD, a     ; RD0
+	CALL    DELAY
+	MOVLW   0x03
+	MOVWF   LATD, a     ; RD0+RD1
+	CALL    DELAY
+	MOVLW   0x07
+	MOVWF   LATD, a     ; RD0+RD1+RD2
+	CALL    DELAY
+	MOVLW   0x0F
+	MOVWF   LATD, a     ; todos
+	CALL    DELAY
+	GOTO    MAIN
+
+    SECUENCIA_3:            ; Mitad interior vs esquinas
+	MOVLW   0x06
+	MOVWF   LATD, a     ; RD1 y RD2 (mitad)
+	CALL    DELAY
+	MOVLW   0x09
+	MOVWF   LATD, a     ; RD0 y RD3 (esquinas)
+	CALL    DELAY
+	GOTO    MAIN
